d3c33d0912642aa94af5e7ef35e33e3ca06a5824
Ben Meredith <ben.meredith@gmail.com>
2019-11-09 17:32:41 -0500

    Make the logo path make sense

--- packages/gatsby-theme-tabor/src/components/Logo.js
+++ packages/gatsby-theme-tabor/src/components/Logo.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import wpgraphqlLogo from '../images/WPStew.png';
+import wpsLogo from '../images/WPStew.png';
 
 const Logo = ({ data }) => (
   <Link to="/" className="custom-logo-link site-logo" rel="home" itemProp="url">
     <img
-      src={wpgraphqlLogo}
-      alt="WPGraphQL Logo"
+      src={wpsLogo}
+      alt="WPS in white all caps lettering on a black circular background"
       width="150"
       height="150"
       className="custom-logo initial loaded"
-      alt={data.site.siteMetadata.title}
       itemProp="logo"
     />
   </Link>
