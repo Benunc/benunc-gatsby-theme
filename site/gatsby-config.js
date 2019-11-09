df109d89845a764a4ca19c1365c2bacf368c391c
Ben Meredith <ben.meredith@gmail.com>
2019-11-09 15:10:04 -0500

    change the social media author's name

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter`,
     description: `Gatsby starter site `,
-    author: `@alexadark`,
-    wordPressUrl: `http://gatsby.wpsteward.com`,
+    author: `@benUNC`,
+    wordPressUrl: `http://www.wpsteward.com`,
   },
   __experimentalThemes: [
     {
